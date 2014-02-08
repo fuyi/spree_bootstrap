279828c94714c85fe3eb71c29734ce3cb0a1773f
fuyi <yi@whaam.com>
2014-02-08 23:15:49 +0100

    copy all spree controller for customization

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -1,10 +1,3 @@
-# module Spree
-#   class HomeController < ActionController::Base
-#     def index
-#       render 'asdfadsfad'
-#     end
-#   end
-# end
 module Spree
   class HomeController < Spree::StoreController
     helper 'spree/products'
